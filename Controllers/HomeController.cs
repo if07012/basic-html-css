28b0b184e409b6634882b168558c0a668cd52b06
cpr <cpr@voxteneo.asia>
2017-04-18 14:06:06 +0700

    Add view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,24 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using basic_html_css.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace basic_html_css.Controllers
 {
     public class HomeController : Controller
     {
+        Context context;
+        public HomeController(Context context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
+
+            return View();
+        }
+        public IActionResult Task()
+        {
+
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Task(basic_html_css.Models.Task task)
+        {
+            context.Tasks.Add(task);
+            context.SaveChanges();
+            return View();
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
+        public IActionResult List()
+        {
 
+            return Json(context.Tasks);
+        }
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
